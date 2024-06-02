48b4def7cd6ae44c41ec979c44267fb2ebbf1139
temaanggaraunsia <temaanggara02@student.unsia.ac.id>
2024-06-02 12:45:30 +0700

    delete

--- index.php
+++ index.php
@@ -2,15 +2,11 @@
 include 'includes/db.php';
 include 'includes/functions.php';
 
-if (isset($_GET['delete_id'])) {
-    $delete_id = $_GET['delete_id'];
-    $sql = "DELETE FROM products WHERE id_barang = $delete_id";
+$message = '';
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Barang berhasil dihapus";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+if (isset($_GET['status']) && isset($_GET['message'])) {
+    $message = $_GET['message'];
+    $status = $_GET['status'];
 }
 
 $products = getAllProducts($conn);
@@ -34,6 +30,11 @@ $products = getAllProducts($conn);
             <a href="increase_quantity.php">Tambah Jumlah Barang</a> |
             <a href="use_product.php">Gunakan Barang</a>
         </nav>
+        <?php if ($message) : ?>
+            <div class="message <?php echo $status === 'success' ? 'success' : 'error'; ?>">
+                <?php echo $message; ?>
+            </div>
+        <?php endif; ?>
         <table>
             <thead>
                 <tr>
@@ -59,7 +60,7 @@ $products = getAllProducts($conn);
                         <td><?php echo $product['status_barang'] ? 'Available' : 'Not Available'; ?></td>
                         <td>
                             <a href="edit_product.php?id=<?php echo $product['id_barang']; ?>">Edit</a> |
-                            <a href="index.php?delete_id=<?php echo $product['id_barang']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus barang ini?')">Hapus</a>
+                            <a href="delete_product.php?id=<?php echo $product['id_barang']; ?>" onclick="return confirm('Apakah Anda yakin ingin menghapus barang ini?')">Hapus</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
